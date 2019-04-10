a6b944962dbb109dbe578db9afc1d129e08e658d
leehwarang <hwrng2@gmail.com>
2019-04-10 17:28:18 +0900

    190410  - 같은 디렉토리 내에 있는 파일의 함수 중 중복되는 함수 제거 후 모듈로 호출

--- cs23/cs23_1/binaryadder/m2.py
+++ cs23/cs23_1/binaryadder/m2.py
@@ -1,19 +1,4 @@
-def half_Adder(bitA, bitB):
-
-    sum = bitA ^ bitB #XOR연산: 두 값이 같으면 0, 같지 않으면 1 반환
-    carryout = bitA & bitB
-    
-    return [sum, carryout]
-
-#전체덧셈(fulladder) : 입력을 두 개와 자리올림 비트를 입력으로 받아서, 합(sum)과 자리올림(carry)를 배열로 리턴하는 함수
-def full_Adder(bitA, bitB, carryIn):
-    result1 = half_Adder(bitA, bitB)
-    result2 = half_Adder(carryIn, result1[0])
-    
-    sum = result2[0]
-    carryout = result2[1] | result1[1]
-
-    return [sum, carryout]
+import m1 
 
 def bit8_Adder(byteA, byteB):
     
@@ -22,13 +7,12 @@ def bit8_Adder(byteA, byteB):
 
     for bitA, bitB in zip(byteA, byteB):
         # print("======bitA:{}, bitB:{}, carryIn:{}===========".format(bitA, bitB, carryIn))
-        result = full_Adder(bitA, bitB, carryIn)
+        result = m1.full_Adder(bitA, bitB, carryIn)
         answer.append(result[0])
         carryIn = result[1]
     answer.append(result[1])
     
     return answer
 
-
-bit8_Adder([1, 1, 0, 1, 1, 0, 1, 0], [1, 0, 1, 1, 0, 0, 1, 1])
-bit8_Adder([1, 1, 0, 0, 1, 0, 1, 0], [1, 1, 0, 1, 1, 0, 0, 1])
+print(bit8_Adder([1, 1, 0, 1, 1, 0, 1, 0], [1, 0, 1, 1, 0, 0, 1, 1]))
+print(bit8_Adder([1, 1, 0, 0, 1, 0, 1, 0], [1, 1, 0, 1, 1, 0, 0, 1]))
