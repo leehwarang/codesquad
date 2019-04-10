5039da4cc206ecbcd0364938047567b88ed4963e
leehwarang <hwrng2@gmail.com>
2019-04-10 17:26:41 +0900

    190410 - xor연산자 사용하던 것을 모듈로 만들어 놓은 xorgate 불러와서 사용하도록 수정

--- cs23/cs23_1/binaryadder/m1.py
+++ cs23/cs23_1/binaryadder/m1.py
@@ -1,15 +1,19 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import gate 
+
 #반쪽덧셈(halfadder) : 입력을 두 개 받아서, 합(sum)과 자리올림(carry)를 배열로 리턴하는 함수
 def half_Adder(bitA, bitB):
-
-    sum = bitA ^ bitB #XOR연산: 두 값이 같으면 0, 같지 않으면 1 반환
+    sum = gate.xorGate(bitA, bitB)
     carryout = bitA & bitB
     
     return [sum, carryout]
 
-print(half_Adder(0, 0))
-print(half_Adder(0, 1))
-print(half_Adder(1, 0))
-print(half_Adder(1, 1))
+# print(half_Adder(0, 0))
+# print(half_Adder(0, 1))
+# print(half_Adder(1, 0))
+# print(half_Adder(1, 1))
 
 #전체덧셈(fulladder) : 두 개의 입력과 자리올림 비트를 입력으로 받아서, 합(sum)과 자리올림(carry)를 배열로 리턴하는 함수
 def full_Adder(bitA, bitB, carryIn):
@@ -21,14 +25,16 @@ def full_Adder(bitA, bitB, carryIn):
 
     return [sum, carryout]
     
-print(full_Adder(0,0,0))
-print(full_Adder(0,0,1))
-print(full_Adder(0,1,0))
-print(full_Adder(1,0,0))
-print(full_Adder(0,1,1))
-print(full_Adder(1,1,0))
-print(full_Adder(1,0,1))
-print(full_Adder(1,1,1))
+# print(full_Adder(0,0,0))
+# print(full_Adder(0,0,1))
+# print(full_Adder(0,1,0))
+# print(full_Adder(1,0,0))
+# print(full_Adder(0,1,1))
+# print(full_Adder(1,1,0))
+# print(full_Adder(1,0,1))
+# print(full_Adder(1,1,1))
+
+
 
 
         
