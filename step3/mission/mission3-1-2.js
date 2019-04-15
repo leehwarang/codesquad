1489eebe820a92cae4dcc35d4340305b0ec55070
leehwarang <hwrng2@gmail.com>
2019-04-15 13:26:32 +0900

    90415 배열과 객체 실습 3-1-2. - json에서 type이 sk인 name을 배열로 출력하기

---
+++ step3/mission/mission3-1-2.js
@@ -0,0 +1,116 @@
+//3-1-2. stype이 sk인, name으로 구성된 배열만 출력해본다.
+
+const data = [
+  {
+    id: 1,
+    name: "Yong",
+    phone: "010-0000-0000",
+    type: "sk",
+    childnode: [
+      {
+        id: 11,
+        name: "echo",
+        phone: "010-0000-1111",
+        type: "kt",
+        childnode: [
+          {
+            id: 115,
+            name: "hary",
+            phone: "211-1111-0000",
+            type: "sk",
+            childnode: [
+              {
+                id: 1159,
+                name: "pobi",
+                phone: "010-444-000",
+                type: "kt",
+                childnode: [
+                  {
+                    id: 11592,
+                    name: "cherry",
+                    phone: "111-222-0000",
+                    type: "lg",
+                    childnode: []
+                  },
+                  {
+                    id: 11595,
+                    name: "solvin",
+                    phone: "010-000-3333",
+                    type: "sk",
+                    childnode: []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            id: 116,
+            name: "kim",
+            phone: "444-111-0200",
+            type: "kt",
+            childnode: [
+              {
+                id: 1168,
+                name: "hani",
+                phone: "010-222-0000",
+                type: "sk",
+                childnode: [
+                  {
+                    id: 11689,
+                    name: "ho",
+                    phone: "010-000-0000",
+                    type: "kt",
+                    childnode: [
+                      {
+                        id: 116890,
+                        name: "wonsuk",
+                        phone: "010-000-0000",
+                        type: "kt",
+                        childnode: []
+                      },
+                      {
+                        id: 1168901,
+                        name: "chulsu",
+                        phone: "010-0000-0000",
+                        type: "sk",
+                        childnode: []
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            id: 117,
+            name: "hong",
+            phone: "010-0000-0000",
+            type: "lg",
+            childnode: []
+          }
+        ]
+      }
+    ]
+  }
+];
+
+let result = [];
+
+const objtypeCheck = function(_array) {
+  _array.forEach(_obj => {
+    //배열 안에 있는 객체들을 순회하면서 검사
+    if (_obj.type === "sk") {
+      result.push(_obj.name);
+    }
+    if (_obj.childnode !== []) {
+      objtypeCheck(_obj.childnode);
+    }
+    if (_obj.childnode === []) {
+      return;
+    }
+  });
+};
+
+objtypeCheck(data);
+
+console.log(result);
