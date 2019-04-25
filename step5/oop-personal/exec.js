b46fe6e656afac84d6fced593b600b8a43721d57
leehwarang <hwrng2@gmail.com>
2019-04-25 22:24:36 +0900

    feat ManageTodo의 delete 함수 구현

--- step5/oop-personal/exec.js
+++ step5/oop-personal/exec.js
@@ -4,8 +4,12 @@ const ManageTodo = require("./managetodo.js");
 const managedTodo = new ManageTodo();
 
 managedTodo.add("자바스크립트 공부하기", ["programming", "javascript"], "todo");
-managedTodo.add("그림 그리기", ["picture", "favorite"], "doing");
+managedTodo.add("그림 그리기", ["picture", "hobby"], "doing");
+managedTodo.add("강아지 산책", ["family", "favorite"], "done");
 
 managedTodo.show("all");
 managedTodo.show("todo");
 managedTodo.show("doing");
+
+managedTodo.delete(2);
+// managedTodo.update(id, )
