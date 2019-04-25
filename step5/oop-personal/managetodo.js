bc45638cc12bd9359d2abe5f7f0a26276408739e
leehwarang <hwrng2@gmail.com>
2019-04-25 22:35:09 +0900

    feat ManageTodo의 update 함수 구현

--- step5/oop-personal/managetodo.js
+++ step5/oop-personal/managetodo.js
@@ -36,8 +36,22 @@ ManageTodo.prototype.add = function(name, tags, status = "todo") {
   //add 함수를 호출하는 실행부가 Mageger의 인스턴스이기 때문에, this는 ManageTodo.prototype이 아닌 인스턴스에 바인딩함
 };
 
-ManageTodo.prototype.delete = function() {};
+ManageTodo.prototype.delete = function(deleteId) {
+  const targetTodo = this.managedTodoList.find(todo => todo.id === deleteId);
+  const targetIndex = this.managedTodoList.findIndex(
+    todo => todo.id === deleteId
+  );
+  this.managedTodoList.splice(1, 1);
+  console.log(
+    `${targetTodo.name}이(가) ${targetTodo.status} 목록에서 삭제 되었습니다.`
+  );
+};
 
-ManageTodo.prototype.update = function() {};
+ManageTodo.prototype.update = function(updateId, changeStatus) {
+  const targetTodo = this.managedTodoList.find(todo => todo.id === updateId);
+  console.log(
+    `${targetTodo.name}의 상태가 ${changeStatus}(으)로 변경 되었습니다.`
+  );
+};
 
 module.exports = ManageTodo;
